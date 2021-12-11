b53b7c5631a8b1dc77838ab96c99e57f46bfee9e
xiaoxiaofei <95118427+xiaoxiaofeixf@users.noreply.github.com>
2021-12-11 21:27:16 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # configs
-AUUID=24b4b1e1-7a89-45f6-858c-242cf53b5bdb
+AUUID=9921c728-5fd3-4aec-94c6-a61d40568d06
 CADDYIndexPage=https://github.com/AYJCSGM/mikutap/archive/master.zip
 CONFIGCADDY=https://raw.githubusercontent.com/Lbingyi/HerokuXray/master/etc/Caddyfile
 CONFIGXRAY=https://raw.githubusercontent.com/Lbingyi/HerokuXray/master/etc/xray.json
